0fbc92fae5a2e243e6bb6f74c14eb1bc1ab695d5
inessa-lee <lee.inessa@yahoo.com>
2016-01-24 15:35:50 +0100

    added recent nav bar and footer

--- kendo.js
+++ kendo.js
@@ -1,22 +1,3 @@
-// $(document).ready(function() {
-//     $('.slideshow').cycle({
-// 		fx: 'fade'
-//     });
-//     $('#swapImage').click(function () {
-//         if ($('#swapImage').attr('src')=='teacher1Norm.jpg'){
-//             $('#swapImage').attr('src','teacher1.jpg');
-//         }
-//         else {
-//             $('#swapImage').attr('src','teacher1Norm.jpg');
-//         }
-//     }); 
-// });
-
-
-
-// The first function below makes the navigation bar slide up and down when scrolling. //
-// Make sure you also copy the css for navbar-fixed! //
-
 $(document).ready(function() {
   
   $(window).scroll(function () {
@@ -29,11 +10,6 @@ $(document).ready(function() {
     };
   });
 
-
-
-// This function (below) adds the 'active1' class to the navigationbar-link //
-// Make sure you also copy in the css for 'active1' //
-
   var path = window.location.href; 
   $('.main-nav a').each(function() {
     if (this.href === path) {
@@ -41,14 +17,11 @@ $(document).ready(function() {
     };
   });
 
-// This function below I copied from Inessa which work really good YAY! //
-
     $('.slideshow').cycle({
       fx: 'fade'
     });
 
-// I haven't tried the other function for the teacher images yet //
-    
+   
     $('#swapImage').click(function () {
       if ($('#swapImage').attr('src')=='teacher1Norm.jpg'){
         $('#swapImage').attr('src','teacher1.jpg');
